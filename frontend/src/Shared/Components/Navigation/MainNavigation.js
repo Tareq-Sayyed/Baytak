271ce1ac6c1a39761a7548210404d7cbd46434cd
Tareq El-Sayyed <tareqelsayyed@gmail.com>
2023-06-29 11:12:29 +0300

    Added the some nav-link sin the ManNavigation component, set up the SideDrawer component which is to be rendered using the createPortal method { made a new div with a new id in th inde.html file. Added some css for the NavLinks.js and the SideDrawer.js files.

--- frontend/src/Shared/Components/Navigation/MainNavigation.js
+++ frontend/src/Shared/Components/Navigation/MainNavigation.js
@@ -1,22 +1,33 @@
-import './MainNavigation.css'
-import MainHeader from './MainHeader';
-import { Link } from 'react-router-dom';
-
+import "./MainNavigation.css";
+import MainHeader from "./MainHeader";
+import { Link } from "react-router-dom";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import React from "react";
 const MainNavigation = () => {
-    return ( 
-        <MainHeader>
-            <button className = "main-navigation__menu-btn">
-                <span />
-                <span />
-                <span />
-            </button>
-            <h1 className = "main-navigation__title">
-                <Link to = "/">Your Places</Link>
-            </h1>
-            <nav>
-            </nav>
-        </MainHeader>     
-     );
-}
- 
+  return (
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
+};
+
 export default MainNavigation;
---
+++ frontend/src/Shared/Components/Navigation/SideDrawer.js
@@ -0,0 +1,10 @@
+import { createPortal } from 'react-dom';
+import './SideDrawer.css';
+
+const SideDrawer = (props) => {
+    const content = <aside className="side-drawer">{props.children}</aside>;
+
+    return createPortal(content, document.getElementById('drawer-hook'));
+}
+ 
+export default SideDrawer
